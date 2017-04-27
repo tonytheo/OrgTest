86f21b3efb3c4534998b3bf2f21eec9963a1c20e
Mr. Theodoropoulos <tony.theodoropoulos@ed.amdsb.ca>
2017-04-27 13:35:31 -0400

    more comments

--- OrgTest/Form1.cs
+++ OrgTest/Form1.cs
@@ -20,6 +20,7 @@ namespace OrgTest
         private void button1_Click(object sender, EventArgs e)
         {
             //event comment
+            //event comment 2
             this.BackColor = Color.DarkRed;
         }
     }
